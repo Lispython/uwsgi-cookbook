df5be1278f1a3b90dd45d06fd3a27948d111d9ed
Alex <Alexandr.S.rus@gmail.com>
2013-09-29 16:32:14 +0400

    Version update

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email "alex@obout.ru"
 license          "BSD, see LICENSE for more details."
 description      "Install/Configures uwsgi spplications"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.0.1"
+version          "0.0.2"
 
 
 %w[ debian ubuntu].each do |os|
